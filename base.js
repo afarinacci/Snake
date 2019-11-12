4d2292db86fadfdc85abdbd8487d052e28fb0eb6
AFarinacci <andreafarinacci@me.com>
2019-11-12 13:25:59 +0700

    Add game functionality

---
+++ base.js
@@ -0,0 +1,7 @@
+const dropFirst = xs => xs.slice(1);
+const dropLast = xs => xs.slice(0, xs.length - 1);
+const rnd = min => max => Math.floor(Math.random() * max) + min;
+const mod = x => y => ((y % x) + x) % x;
+const merge = o1 => o2 => Object.assign({}, o1, o2);
+
+module.exports = { dropFirst, dropLast, rnd, mod, merge };
